6fad88c56fd2b7cee42011a546acdda31e6d6d2f
SeungWookHan <hswook12@me.com>
2022-12-05 23:34:42 +0900

    Change member repository to SpringDataJpaMemberRepository

--- hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -13,6 +13,7 @@ import javax.swing.*;
 @Configuration
 public class SpringConfig {
 
+    // JDBC 관련
 //    private DataSource dataSource;
 //
 //    @Autowired
@@ -20,23 +21,33 @@ public class SpringConfig {
 //        this.dataSource = dataSource;
 //    }
 
-    private EntityManager em;
+    // JPA 관련
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    // 스프링 데이터 JPA
+    private final MemberRepository memberRepository;
+
+    @Autowired // 생성자가 하나일때는 생략가능
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+//        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateMemberRepository(dataSource);
+////        return new JpaMemberRepository(em);
+//    }
 }
